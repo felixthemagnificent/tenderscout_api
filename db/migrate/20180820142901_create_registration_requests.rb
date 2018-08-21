8015c3b48589dbecff0226a6fcba3c1dc4c253ac
Dmytro Piskovyi <dmytro.piskovyi@masterofcode.com>
2018-08-21 15:53:15 +0300

    updating migration file to data types

--- db/migrate/20180820142901_create_registration_requests.rb
+++ db/migrate/20180820142901_create_registration_requests.rb
@@ -4,20 +4,20 @@ class CreateRegistrationRequests < ActiveRecord::Migration[5.1]
       t.string :fullname, null: false, default: ''
       t.string :company, null: false, default: ''
       t.integer :company_size, null: false, default: 0
-      t.string :state
-      t.string :country
-      t.string :city
-      t.string :sector
-      t.string :turnover
-      t.json :markets
-      t.integer :tender_level
-      t.string :win_rate
-      t.integer :number_public_contracts
-      t.json :do_use
-      t.boolean :do_collaborate
-      t.float :tender_complete_time
-      t.integer :organisation_count
-      t.boolean :do_processed
+      t.string :state, null: false, default: ''
+      t.string :country, null: false, default: ''
+      t.string :city, null: false, default: ''
+      t.string :sector, null: true
+      t.integer :turnover, null: false, default: 0
+      t.json :markets, null: false, default: {}
+      t.integer :tender_level, null: false, default: 0
+      t.float :win_rate, null: false, default: 0.00
+      t.integer :number_public_contracts, null: false, default: ''
+      t.json :do_use, null: false, default: false
+      t.boolean :do_collaborate, null: false, default: false
+      t.float :tender_complete_time, null: false, default: 0.00
+      t.integer :organisation_count, null: false, default: 0
+      t.boolean :do_processed, null: false, default: false
       t.timestamps
 
       t.references :country, foreign_key: true
