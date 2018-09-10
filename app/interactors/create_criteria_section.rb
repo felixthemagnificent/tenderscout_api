f082765142fc5abe9cd94c339c3d6acf36b75a62
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-09-10 11:12:19 +0500

    fix

--- app/interactors/create_criteria_section.rb
+++ app/interactors/create_criteria_section.rb
@@ -6,24 +6,33 @@ class BulkCreateCriteriaSection
       context.fail! errors: { error: :unauthorized, error_description: 'Action is not allowed'},
                       code: :unauthorized unless context.user.admin?
 
-      section = context.tender.criteries.new(marketplace_tender_criteria_section_params)
+      section = context.tender.criteria_sections.new(marketplace_tender_criteria_section_params)
       section.save!
 
-      context.params.criteries
+      create_criteries(marketplace_tender_criteria_params)
       
     end
   end
 
   private
 
-  def create_criteria
+  def create_criteries(params: nil, parent: nil)
+    params[:criteries].each do |e|
+      criteria = section.criteries.create(
+        order: e[:order],
+        title: e[:title],
+        parent: parent
+        )
+      create_criteries(params: e[:criteries], parent: criteria) if e[:criteries]
+    end
+    section.criteries.create(order: order, )
   end
 
   def marketplace_tender_criteria_section_params
     context.params.permit(:order, :title)
   end
 
-  def marketplace_tender_criteria_section_params
+  def marketplace_tender_criteria_params
     context.params.permit(:order, :title, :tender_id, criteries: [])
   end
 
