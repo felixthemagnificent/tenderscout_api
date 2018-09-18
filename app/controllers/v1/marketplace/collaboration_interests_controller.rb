39a6833ad8a91063656fbbc15e5379819bec02ab
Dmytro Piskovyi <dmytro.piskovyi@masterofcode.com>
2018-09-18 22:53:15 +0300

    controller fix for collaboration

--- app/controllers/v1/marketplace/collaboration_interests_controller.rb
+++ app/controllers/v1/marketplace/collaboration_interests_controller.rb
@@ -1,4 +1,4 @@
-class V1::CollaborationInterestsController < ApplicationController
+class V1::Marketplace::CollaborationInterestsController < ApplicationController
   include ActionController::Serialization
   before_action :set_collaboration_interest, only: [:show, :destroy]
 
