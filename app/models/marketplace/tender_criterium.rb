c30a08688e386c2493abc8ecb6620f43f45fe99b
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-09-21 14:21:26 +0500

    fixes

--- app/models/marketplace/tender_criterium.rb
+++ app/models/marketplace/tender_criterium.rb
@@ -1,4 +1,5 @@
 class Marketplace::TenderCriterium < ApplicationRecord
+  has_one :tender, through: :section
   belongs_to :section, class_name: 'Marketplace::TenderCriteriaSection', foreign_key: :section_id
   has_many :subcriterias, class_name: 'Marketplace::TenderCriterium', foreign_key: :parent_id
   belongs_to :parent, class_name: 'Marketplace::TenderCriterium', optional: true
