6b31ce46f65e8c66d2bfe9ebb4b18d36ebb75238
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-09-25 17:17:13 +0500

    fix pagination in codes search

--- app/models/industry_code.rb
+++ app/models/industry_code.rb
@@ -3,9 +3,6 @@ class IndustryCode < ApplicationRecord
   belongs_to :industry
 
   def self.search_codes(code)
-    results = TenderCodesIndex.query(match:{ code: code} )
-    count = results.count
-    results = results.objects.page(paginate_params[:page]).per(paginate_params[:page_size])
-    render json: {data: results, count: count}
+    TenderCodesIndex.query(match:{ code: code} )
   end
 end
