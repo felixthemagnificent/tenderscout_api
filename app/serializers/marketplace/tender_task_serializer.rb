162befa425f477f7db6cc475d3d9eb4d53f814bb
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-10-02 19:29:11 +0500

    fix award criteria answer

--- app/serializers/marketplace/tender_task_serializer.rb
+++ app/serializers/marketplace/tender_task_serializer.rb
@@ -1,4 +1,4 @@
 class Marketplace::TenderTaskSerializer < ActiveModel::Serializer
-  attributes :id, :order, :title, :weight, :description
+  attributes :id, :order, :title, :weight, :description, :parent_id
   has_many :answers
 end
