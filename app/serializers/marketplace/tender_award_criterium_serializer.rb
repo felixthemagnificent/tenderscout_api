146b42d5cb54889ed9d10c63cff3d8ff9bb88ad6
edzyo <makogin1995@gmail.com>
2018-10-08 15:24:12 +0300

    [+] add deadline field to (tasks,award_criteria), create update method

--- app/serializers/marketplace/tender_award_criterium_serializer.rb
+++ app/serializers/marketplace/tender_award_criterium_serializer.rb
@@ -1,4 +1,4 @@
 class Marketplace::TenderAwardCriteriumSerializer < ActiveModel::Serializer
-  attributes :id, :order, :title, :description, :weight
+  attributes :id, :order, :title, :description, :weight, :deadline
   has_many :answers, serializer: Marketplace::TenderAwardCriteriumAnswerSerializer
 end
