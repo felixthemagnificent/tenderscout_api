ae782077d77e4c4b3198a5fe7290ce0f4e4ccc0a
edzyo <makogin1995@gmail.com>
2018-10-11 13:20:57 +0300

    [+] fixed comments destroy

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   belongs_to :profile
-  has_many :childrens, class_name: 'Comment', foreign_key: "parent_id"
+  has_many :childrens, class_name: 'Comment', foreign_key: "parent_id", dependent: :destroy
 
   COMMENT_MODEL= {
       'tender_task' => 'Marketplace::TenderTask',
