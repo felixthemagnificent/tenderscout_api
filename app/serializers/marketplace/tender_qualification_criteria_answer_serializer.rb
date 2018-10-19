a337f278ebacd1f7e005d82a89fbab71cded056f
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-10-19 13:06:46 +0500

    add created_at to answers

--- app/serializers/marketplace/tender_qualification_criteria_answer_serializer.rb
+++ app/serializers/marketplace/tender_qualification_criteria_answer_serializer.rb
@@ -1,4 +1,4 @@
 class Marketplace::TenderQualificationCriteriaAnswerSerializer < ActiveModel::Serializer
-  attributes :id, :pass_fail, :score, :closed
+  attributes :id, :pass_fail, :score, :closed, :created_at
   has_one :user, serializer: UserSerializer
 end
