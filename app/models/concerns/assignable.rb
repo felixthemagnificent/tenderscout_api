046a140d760cff2369441cb8e43886348f37aa42
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-10-22 13:53:13 +0500

    fix

--- app/models/concerns/assignable.rb
+++ app/models/concerns/assignable.rb
@@ -6,14 +6,14 @@ module Assignable
       section = self.section
       return nil unless user_id.present?
       collaboration = Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: section.tender.id), user: user_id).first.collaboration
-      self.assignments.where(collaboration: collaboration).first.user.profiles.first
+      self.assignments.where(collaboration: collaboration).first.user.profiles.first rescue nil
     end
 
     def assignment
       section = self.section
       return nil unless user_id.present?
       collaboration = Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: section.tender.id).ids, user: user_id).first.collaboration
-      self.assignments.where(collaboration: collaboration).first
+      self.assignments.where(collaboration: collaboration).first rescue nil
     end
   end
 end
