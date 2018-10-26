76bf5bbfa5663d6b55ee0baf42b8d517171441bc
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-10-26 18:12:20 +0500

    fix

--- config/routes.rb
+++ config/routes.rb
@@ -32,7 +32,7 @@ Rails.application.routes.draw do
       # namespace :compete do
       #   resources :bid_no_bid_answers
       # end
-      get :invites, to: 'collaborations#invites'
+      get :invites, to: 'users#invites'
       get :requests, to: 'user#requests'
       resources :bid_no_bid_answers
       resources :bid_no_bid_questions do
