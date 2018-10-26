71a26e3779a3058eeed5fe5c1bcfdf3d5ec718ac
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-10-26 14:10:37 +0500

    fix award criteria delete

--- app/models/marketplace/tender_award_criterium.rb
+++ app/models/marketplace/tender_award_criterium.rb
@@ -1,9 +1,9 @@
 class Marketplace::TenderAwardCriterium < ApplicationRecord
   belongs_to :section, class_name: 'Marketplace::TenderAwardCriteriaSection', foreign_key: :section_id
-  has_many :answers, class_name: 'Marketplace::TenderAwardCriteriaAnswer', foreign_key: :tender_award_criteria_id
-  has_many :comments, as: :commentable
-  has_many :notes, as: :notable
-  has_many :assignments, class_name: 'Marketplace::Assignment', as: :assignable
+  has_many :answers, class_name: 'Marketplace::TenderAwardCriteriaAnswer', foreign_key: :tender_award_criteria_id, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
+  has_many :notes, as: :notable, dependent: :destroy
+  has_many :assignments, class_name: 'Marketplace::Assignment', as: :assignable, dependent: :destroy
   attr_accessor :user_id
   include Assignable
 end
