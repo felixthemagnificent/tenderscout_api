2bab2a029ec69f48a9e7989e7fb5bd2526c3a94e
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-11-08 10:16:54 +0500

    TEN-398

--- app/serializers/marketplace/compete/bid_no_bid_answer_serializer.rb
+++ app/serializers/marketplace/compete/bid_no_bid_answer_serializer.rb
@@ -1,5 +1,13 @@
 class Marketplace::Compete::BidNoBidAnswerSerializer < ActiveModel::Serializer
-  attributes :id, :answer_text, :comment
+  attributes :id, :answer_text, :comment, :bid_no_bid_question_id, :created_at, :order, :weight
   has_one :bid_no_bid_answer
   has_one :bid_no_bid_question
+
+  def order
+    object.try(:bid_no_bid_answer).try(:order)
+  end
+
+  def weight
+    object.try(:bid_no_bid_answer).try(:weight)
+  end
 end
