af9391bb99588614f73bed10b7799cf23cf1bfc5
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-11-13 18:21:31 +0500

    fix

--- config/initializers/postmark_mailer.rb
+++ config/initializers/postmark_mailer.rb
@@ -1,5 +1,4 @@
 PostmarkMailer.configure do |config|
   # config.api_key = Rails.application.secrets.postmark_api_token
-  config.api_key = "2f791fbd-8c09-40a5-97f3-40d1138baa3f"
   config.default_delivery_queue = :mailers
 end
