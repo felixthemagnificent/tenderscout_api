3f6f8e89f6f3809a7e69316f421c6174456840d7
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-11-14 16:11:52 +0500

    fix

--- app/mailers/custom_postmark_mailer.rb
+++ app/mailers/custom_postmark_mailer.rb
@@ -1,10 +1,10 @@
 class CustomPostmarkMailer < PostmarkMailer::Base
   def template_email(email_to, template_id, model)
     mail(
-      from: Rails.configuration.mailer['from'],
-      to: email_to,
-      template_id: template_id,
-      template_model: model
+      From: Rails.configuration.mailer['from'],
+      To: email_to,
+      TemplateId: template_id,
+      TemplateModel: model
     )
   end
 end
