4b81fd7139c7cdafb43b87596d1f775e7027106e
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-11-14 14:53:16 +0500

    fix sidekiq starting

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,3 +1,4 @@
 server "ec2-34-238-233-234.compute-1.amazonaws.com", user: "deploy", roles: %w{app db web}
 set :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 set :stage, :alpha_staging
+set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
