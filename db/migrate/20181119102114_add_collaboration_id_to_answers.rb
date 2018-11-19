ab6c2fd9fc679f7c240ae24b9a3983280b1e0479
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-11-20 01:56:18 +0500

    fix

--- db/migrate/20181119102114_add_collaboration_id_to_answers.rb
+++ db/migrate/20181119102114_add_collaboration_id_to_answers.rb
@@ -1,12 +1,12 @@
 class AddCollaborationIdToAnswers < ActiveRecord::Migration[5.1]
   def up
     Marketplace::TenderAwardCriteriaAnswer.where(collaboration_id: nil).each do |answer|
-      collaboration =  Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: answer.tender_id), user: answer.user_id).first.collaboration
-      answer.update(collaboration_id: collaboration.id)
+      collaboration =  Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: answer.tender_id), user: answer.user_id).try(:first).try(:collaboration)
+      answer.update(collaboration_id: collaboration.id) if collaboration
     end
     Marketplace::TenderQualificationCriteriaAnswer.where(collaboration_id: nil).each do |answer|
-      collaboration =  Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: answer.tender_id), user: answer.user_id).first.collaboration
-      answer.update(collaboration_id: collaboration.id)
+      collaboration =  Marketplace::TenderCollaborator.where(collaboration: Marketplace::Collaboration.where(tender: answer.tender_id), user: answer.user_id).try(:first).try(:collaboration)
+      answer.update(collaboration_id: collaboration.id) if collaboration
     end
   end
 end
