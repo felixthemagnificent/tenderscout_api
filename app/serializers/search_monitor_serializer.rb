13ab9e03afb78fa1cb13507980559f5287e568c2
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-12-12 13:18:15 +0500

    add monitor_type to search monitor serializer

--- app/serializers/search_monitor_serializer.rb
+++ app/serializers/search_monitor_serializer.rb
@@ -1,6 +1,6 @@
 class SearchMonitorSerializer < ActiveModel::Serializer
   attributes :id, :title, :tenderTitle, :countryList, :keywordList, :valueFrom, :valueTo, :codeList, :buyer,
-             :statusList, :is_archived, :status, :tenders_count, :submission_date_to, :submission_date_from
+             :statusList, :is_archived, :status, :tenders_count, :submission_date_to, :submission_date_from, :monitor_type
 
   attribute(:is_favourite) { is_favourite_monitor?(object)}
 
