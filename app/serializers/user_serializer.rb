83d4c28e59e5ebed83190ce33180446ac85aa5c9
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2018-12-14 15:25:10 +0500

    add flags to serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :role
+  attributes :id, :email, :role, :flags
   attribute(:profile_type) { object.profiles.try(:first).try(:profile_type) }
   has_many :profiles, serializer: ProfileSerializer
 end
