36b7e130fc22f64284aa11c1618a193b2df2c586
edzyo <makogin1995@gmail.com>
2018-12-18 10:59:29 +0200

    [+] add docx to format

--- app/models/tender_collaboration_document.rb
+++ app/models/tender_collaboration_document.rb
@@ -2,7 +2,6 @@ class TenderCollaborationDocument < ApplicationRecord
   belongs_to :tender, class_name: 'Core::Tender'
   mount_uploader :file, TenderCollaborationDocumentUploader
   validates_presence_of :file
-  before_create :file_size_validation#, file_size: { less_than: 1.megabytes }
   before_save :update_file_attributes
 
   private
