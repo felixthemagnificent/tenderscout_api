22f0409131f91a3fa094f95db166e80f9725fe4b
edzyo <makogin1995@gmail.com>
2018-12-21 12:59:40 +0200

    [~] rewrite bid result response

--- app/serializers/tender_bid_result_serializer.rb
+++ app/serializers/tender_bid_result_serializer.rb
@@ -1,15 +1,4 @@
 class TenderBidResultSerializer < ActiveModel::Serializer
-  attribute(:award_criteria_section) do
-  object
-  end
-  attribute(:award_criteries) do
-    object.try(:award_criteries)
-  end
-  attribute(:bid_results) do
-    bid_result = []
-    object.try(:award_criteries).each do |criteria|
-     bid_result << criteria.try(:marketplace_bid_results) if criteria.marketplace_bid_results.any?
-    end
-    bid_result.flatten
-  end
+  attributes :id, :estimate_score, :actual_score, :winning_score, :tender_award_criteria_id,
+             :created_at, :updated_at
 end
