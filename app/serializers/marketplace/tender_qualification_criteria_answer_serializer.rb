926a7042bf473a4afb6453c2ef3b9d285b1017de
edzyo <makogin1995@gmail.com>
2018-12-26 16:47:23 +0200

    [~] add updated_at to criteries new

--- app/serializers/marketplace/tender_qualification_criteria_answer_serializer.rb
+++ app/serializers/marketplace/tender_qualification_criteria_answer_serializer.rb
@@ -1,4 +1,4 @@
 class Marketplace::TenderQualificationCriteriaAnswerSerializer < ActiveModel::Serializer
-  attributes :id, :pass_fail, :score, :closed, :created_at, :collaboration_id
+  attributes :id, :pass_fail, :score, :closed, :created_at, :updated_at, :collaboration_id
   has_one :user, serializer: UserSerializer
 end
