a63cbc0ef7233615ea5a627e3579d001bd8b6632
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-01-17 16:24:07 +0500

    add xlsx

--- app/uploaders/tender_collaboration_document_uploader.rb
+++ app/uploaders/tender_collaboration_document_uploader.rb
@@ -41,7 +41,7 @@ class TenderCollaborationDocumentUploader < CarrierWave::Uploader::Base
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
    def extension_whitelist
-     %w(pdf doc docx xls)
+     %w(pdf doc docx xls xlsx)
    end
 
   # Override the filename of the uploaded files:
