6998de8c5a3f56eb6744e863e92cbb4b68a34e69
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-01-18 19:22:43 +0500

    fix scrapper starting

--- app/jobs/scrapers/scrape_tenders_job.rb
+++ app/jobs/scrapers/scrape_tenders_job.rb
@@ -1,16 +1,12 @@
-class Scrapers::ScrapeTenders < ApplicationJob
+class Scrapers::ScrapeTendersJob < ApplicationJob
   queue_as :scrapers
 
   def perform
-    available_scrapers.each do |scraper|
-      scraper_klass = scraper[:klass]
-      scraper.run_at.each do |run_time|
-        run_time = Chronic.parse(run_time)
-        if ((DateTime.now - 2.minute < run_time) and (run_time < DateTime.now + 2.minutes))
-          scraper_klass.constantize.perform_later unless is_running?(scraper_klass)
-        end
-      end
+    return if is_running?('Scrapers::ScrapeTendersJob') or is_scheduled('Scrapers::ScrapeTendersJob')
+    available_scrapers.each do |scraper_klass|
+      scraper_klass.constantize.perform_later unless is_running?(scraper_klass) or is_scheduled?(scraper_klass)
     end
+    Scrapers::ScrapeTendersJob.perform_later
   end
 
   private
@@ -20,24 +16,11 @@ class Scrapers::ScrapeTenders < ApplicationJob
     job_classes.include?(scraper_klass)
   end
 
+  def is_scheduled?(scraper_klass)
+    Sidekiq::ScheduledSet.new.any? {|job| job.klass == e }
+  end
+
   def available_scrapers
-    [
-      { 
-        klass: 'Scrapers::EbrdJob',
-        run_at:['7am', '12pm', '5pm']
-      },
-      {
-        klass: 'Scrapers::FboGovJob',
-        run_at: ['7am', '12pm', '5pm']
-      },
-      {
-        klass: 'Scrapers::BuyAndSellJob',
-        run_at: ['7am', '12pm', '5pm']
-      },
-      {
-        klass: 'Scrapers::BuyAndSellAwards',
-        run_at: ['7am', '12pm', '5pm']
-      },
-    ]
+    ['Scrapers::EbrdJob', 'Scrapers::FboGovJob', 'Scrapers::BuyAndSellJob', 'Scrapers::BuyAndSellAwards']
   end
 end
--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,14 @@
-Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0' }
-end
+require 'sidekiq'
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379/0' }
+if defined?(Sidekiq)
+  Sidekiq.configure_client do |config|
+    config.redis = { url: 'redis://127.0.0.1:6379/0' }
+  end
+
+  Sidekiq.configure_server do |config|
+    config.redis = { url: 'redis://127.0.0.1:6379/0' }
+    config.on(:startup) do
+      Scrapers::ScrapeTendersJob.perform_later
+    end
+  end
 end
