9146cc40d27f9a90a607515709143ab0d8ed3a65
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-01-23 17:20:32 +0500

    fix

--- app/controllers/v1/scrapper/untitled.rb
+++ app/controllers/v1/scrapper/untitled.rb
@@ -1,20 +1,31 @@
 
   def tender_bulk_upload
     index = 0
-    tenders = Origin::Web::Tender.where('id > ?', 8410436)
+    records = []
+    tenders = Tender.all #.desc('created_at').limit(1).to_a
     tenders.each do |e|
       index += 1
       puts '*'*20
       puts "#{index}/#{tenders.count}"
-      tender_send(e)
+      records << tender_prepare(e)
+      if records.count > 1000
+        tender_send records
+        records.clear
+      end
     end
+    return nil
   end
 
-  def tender_send(tender)
+  def tender_prepare(tender)
     tender_hash = tender.attributes
     tender_hash.merge!({name: tender.try(:organization).try(:name), country_name: tender.try(:country).try(:name)})
-    tender_hash['tender_urls'] = tender_hash['tender_urls'].first
-    tender_hash['origin_id'] = tender_hash['spider_id'].to_s
+    tender_hash['tender_urls'] = ['tender_urls'][0]['url']
+    tender_hash
+  end
+
+  def tender_send(tenders)
+    tender_hash = {required: tenders}
+    # tender_hash['origin_id'] = tender_hash['spider_id'].to_s
     uri = URI('https://hub.tenderscout.com/api/v1/scrapper/input')
     http = Net::HTTP.new(uri.host, uri.port)
     http.use_ssl = true
@@ -27,3 +38,5 @@
   rescue => e
     puts "failed #{e}"
   end
+
+tender_bulk_upload
