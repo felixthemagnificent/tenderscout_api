5f144d5a69d98852882ba3d46e8d594eccbf360c
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-01-24 11:54:41 +0500

    fix

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -38,6 +38,17 @@ class Profile < ApplicationRecord
     self.where(id: ids)
   end
 
+  scope :by_name, ->(user_name) { where("fullname LIKE ?","%#{user_name}%"}
+    keyword_profile_ids = []
+    keywords.each do |e|
+      keyword_profile_ids << Keyword.where(name: e).try(:first).try(:profiles).try(:ids)
+    end 
+    keyword_profile_ids.flatten!
+    keyword_profile_ids.uniq!
+    ids = self.ids & keyword_profile_ids
+    self.where(id: ids)
+  end
+
   def owner?(current_user)
     user == current_user
   end
--- app/serializers/profile_serializer.rb
+++ app/serializers/profile_serializer.rb
@@ -7,7 +7,7 @@ class ProfileSerializer < ActiveModel::Serializer
   belongs_to :industry, serializer: IndustrySerializer
 
   attributes :id, :fullname, :display_name, :avatar, :cover_img, :city,
-             :do_marketplace_available, :profile_type, :company, :company_size,
+             :profile_type, :company, :company_size,
              :valueFrom, :valueTo, :turnover, :timezone, :tender_level,
              :industry, :industries, :country, :description,
              :number_public_contracts, :contacts, :keywords, :countries, 
@@ -15,6 +15,7 @@ class ProfileSerializer < ActiveModel::Serializer
 
   attribute(:values) { [object.try(:valueFrom), object.try(:valueTo)] }
   attribute(:user_id) { object.try(:user).try(:id) }
+  attribute(:do_marketplace_available) { object.try(:user).try(:available?) }
   attribute(:role) { object.try(:user).try(:role) }
   attribute(:email) { object.try(:user).try(:email) }
 end
