ac1edb3d3c5ad095f052837ffe888c2d1028ee8f
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-02-07 10:31:08 +0500

    fix

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,6 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :role, :flags
+  attribute(:do_marketplace_available) { object.available? }
   attribute(:profile_type) { object.profiles.try(:first).try(:profile_type) }
   has_many :profiles, serializer: ProfileSerializer
 end
