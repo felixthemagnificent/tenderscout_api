79e191b7d52f5968fdab642eb337b50005f1da13
Felix Yasnopolski <felix.yasnopolski@gmail.com>
2019-02-15 20:45:24 +0500

    fix apply collaboration

--- app/policies/marketplace/collaboration_policy.rb
+++ app/policies/marketplace/collaboration_policy.rb
@@ -8,7 +8,7 @@ class Marketplace::CollaborationPolicy < Core::TenderPolicy
   
   def apply?
    @collaboration.tender.owner?(@user) || @collaboration.tender_collaborators.where(user: @user).try(:first).try(:role) ||
-        @user.admin? || @user.standard?
+        @user.admin? || @user.standard? || @user.has_collaboration_on_tender?(@collaboration.try(:tender))
   end
 
   def remove?
